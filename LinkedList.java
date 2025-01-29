e98600d9c3f5345ce3907b25fe836491f0049f6d
1U23IT011 <141386217+1U23IT011@users.noreply.github.com>
2025-01-29 09:03:48 +0000

    done7

--- LinkedList.java
+++ LinkedList.java
@@ -69,6 +69,41 @@ public class LinkedList {
         return count;
     }
 
+    public void deleteNode(int position) {
+        // If the position is invalid (less than 1 or greater than the list size)
+        if (position < 1 || position > countNodes()) {
+            System.out.println("Invalid position for deletion.");
+            return;
+        }
+
+        // If deleting the first node
+        if (position == 1) {
+            head = head.next;
+            if (head == null) {
+                tail = null; // If the list becomes empty
+            }
+            return;
+        }
+
+        // If deleting the last node
+        if (position == countNodes()) {
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next;
+            }
+            current.next = null;
+            tail = current;
+            return;
+        }
+
+        // Deleting a node in the middle
+        Node current = head;
+        for (int i = 1; i < position - 1; i++) {
+            current = current.next;
+        }
+        current.next = current.next.next;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         Scanner sc = new Scanner(System.in);
@@ -83,25 +118,45 @@ public class LinkedList {
             } else {
                 String temp = sc.next();
                 System.out.println("LINKED LIST CREATED");
-                System.out.println("LINKED LIST BEFORE INSERTING");
+                System.out.println("LINKED LIST BEFORE INSERTING/DELETING");
                 list.displayList();
 
-                System.out.println("ENTER THE POSITION WHERE YOU WANT TO INSERT");
-                if (sc.hasNextInt()) {
-                    pos = sc.nextInt();
+                System.out.println("1. Insert\n2. Delete");
+                System.out.print("Enter your choice: ");
+                int choice = sc.nextInt();
 
-                    System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
+                if (choice == 1) {
+                    System.out.println("ENTER THE POSITION WHERE YOU WANT TO INSERT");
                     if (sc.hasNextInt()) {
-                        data1 = sc.nextInt();
-                        list.insertMiddle(data1, pos);
-                        System.out.println("LINKED LIST AFTER INSERTING");
+                        pos = sc.nextInt();
+
+                        System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
+                        if (sc.hasNextInt()) {
+                            data1 = sc.nextInt();
+                            list.insertMiddle(data1, pos);
+                            System.out.println("LINKED LIST AFTER INSERTING");
+                            list.displayList();
+                        } else {
+                            System.out.println("Enter integers only");
+                            System.exit(0);
+                        }
+                    } else {
+                        System.out.println("Enter integers only");
+                        System.exit(0);
+                    }
+                } else if (choice == 2) {
+                    System.out.println("ENTER THE POSITION WHERE YOU WANT TO DELETE");
+                    if (sc.hasNextInt()) {
+                        pos = sc.nextInt();
+                        list.deleteNode(pos);
+                        System.out.println("LINKED LIST AFTER DELETING");
                         list.displayList();
                     } else {
                         System.out.println("Enter integers only");
                         System.exit(0);
                     }
                 } else {
-                    System.out.println("Enter integers only");
+                    System.out.println("Invalid choice.");
                     System.exit(0);
                 }
             }
